d89118bb4c7d3a4ced7d54d5340ed4c2442e232a
dazzlingtea <kitsnr08@gmail.com>
2024-08-02 10:18:45 +0900

    auto-deploy setting

---
+++ src/main/java/com/study/event/api/HomeController.java
@@ -0,0 +1,13 @@
+package com.study.event.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "자동배포 확인!!";
+    }
+}
